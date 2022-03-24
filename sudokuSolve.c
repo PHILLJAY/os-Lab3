9417526f458fa25dfea6e4a74a5b03015f37c13a
PHILLJAY <philip.jasionowski@gmail.com>
2022-03-23 22:08:23 -0400

    something works now? idk the read is not reading per se

--- sudokuSolve.c
+++ sudokuSolve.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <stdlib.h>
+
+#define N 9
 
 void print(int arr[N][N])
 {
@@ -16,6 +19,27 @@ void print(int arr[N][N])
 int main (int argc, char *argv[])
 {
 //temporary array until the read function is made;
+	FILE *fp;
+	fp = fopen("Lab3 puzzle.txt", "r");//opens puzzle in read mode
+
+	if (fp == NULL)
+	{
+		perror("Error while opening file.\n");
+		exit(0);
+	}
+	int sudokuArray2[9][9];
+	
+	for(int i = 0; i < 9; i++)
+	{
+		for (int j = 0; j < 9; j++)
+		{
+			
+			fscanf(fp, "%d", sudokuArray2[i][j]);
+			printf("%d", sudokuArray2[i][j]);
+			printf("\n");
+		}
+
+	}
 	int sudokuArray[9][9]={
 	{5,3,4,6,7,8,9,1,2},
 	{6,7,2,1,9,5,3,4,8},
@@ -27,7 +51,8 @@ int main (int argc, char *argv[])
 	{2,8,7,4,1,9,6,3,5},
 	{3,4,5,2,8,6,1,7,9},
 	};
-	
+	print(sudokuArray2);
+
 }
 
 int checkSudoku (int puzzle[9][9])
