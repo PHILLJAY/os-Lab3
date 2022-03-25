1206eaaf919f5c29aa4d7dc4afac12c3fe30e35a
LowKeyFire <robinsonwb7@gmail.com>
2022-03-24 21:29:46 -0400

    X & Y Checker Update
    Functions created for checking columns and rows

--- sudokuSolve.c
+++ sudokuSolve.c
@@ -18,14 +18,47 @@ void print(int arr[9][9])
 //threads down below
 void *checkThreadY(int puzzle[9][9])
 {
-	int tempArray[9];
-	printf("I am the Y checkr thread");
+	printf("I am the Y checker thread");
+	
+	int puzzle[9][9], i, j, k, total, n(9)
+	for (j=1;j<=n;j++) {
+		for(k=j;k==j;k++) {
+			total=0;
+			for (i=1;i<=n;i++) {
+				total = total + puzzle[i][j];
+				}
+		if (total!=45) //column must equal 45 as 1+2+3+4+5+6+7+8+9=45
+			printf("Current Y solution not correct");
+			exit(0);
+			}
+			else{
+				printf("Y solution correct");
+				return 0;
+			}
+        }
 }
 
 void *checkThreadX(int puzzle[9][9])
 {
-	int tempArray[9];
-	printf("I am the X checkr thread");
+	printf("I am the X checker thread");
+	
+	int puzzle[9][9], i, j, k, total, n(9);
+	for (i=1;i<=n;i++) {
+		for (k=i;k==i;k++) {
+			total=0;
+			for (j=1;j<=n;j++) {
+				total = total + puzzle[i][j];
+				}
+
+			if(total!=45) { //row must equal 45 as 1+2+3+4+5+6+7+8+9=45
+				printf("Current X solution not correct");
+				exit(0);
+			}
+			else{
+				printf("X solution correct");
+				return 0;
+			}
+        }
 }
 
 void *checkThreadSquare(int puzzle[9][9])
